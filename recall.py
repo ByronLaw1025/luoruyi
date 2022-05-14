e7711fd7c83b0af171429d24197251af2258502d
ByronLaw1025 <58861048+ByronLaw1025@users.noreply.github.com>
2022-05-14 20:14:00 +0800

    Update recall.py

--- recall.py
+++ recall.py
@@ -19,3 +19,58 @@ from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from deepmatch.models import *
 from deepmatch.utils import sampledsoftmaxloss
 warnings.filterwarnings('ignore')
+print('import success')
+
+# path
+data_path = './tcdata/'
+save_path = './temp_results/'
+# 做召回评估的一个标志, 如果不进行评估就是直接使用全量数据进行召回
+metric_recall = False
+print('path success')
+
+# 读取点击数据，这里分成线上和线下，如果是为了获取线上提交结果应该讲测试集中的点击数据合并到总的数据中
+# 如果是为了线下验证模型的有效性或者特征的有效性，可以只使用训练集
+def get_all_click_df(data_path='./data_raw/', offline=True):
+    if offline:
+        all_click = pd.read_csv(data_path + 'train_click_log.csv')
+    else:
+        trn_click = pd.read_csv(data_path + 'train_click_log.csv')
+        tst_click = pd.read_csv(data_path + 'testA_click_log.csv')
+
+        all_click = trn_click.append(tst_click)
+    
+    all_click = all_click.drop_duplicates((['user_id', 'click_article_id', 'click_timestamp']))
+    return all_click
+
+  # 读取文章的基本属性
+def get_item_info_df(data_path):
+    item_info_df = pd.read_csv(data_path + 'articles.csv')
+    
+    # 为了方便与训练集中的click_article_id拼接，需要把article_id修改成click_article_id
+    item_info_df = item_info_df.rename(columns={'article_id': 'click_article_id'})
+    
+    return item_info_df
+
+# 读取文章的Embedding数据
+def get_item_emb_dict(data_path):
+    item_emb_df = pd.read_csv(data_path + 'articles_emb.csv')
+    
+    item_emb_cols = [x for x in item_emb_df.columns if 'emb' in x]
+    item_emb_np = np.ascontiguousarray(item_emb_df[item_emb_cols])
+    # 进行归一化
+    item_emb_np = item_emb_np / np.linalg.norm(item_emb_np, axis=1, keepdims=True)
+
+    item_emb_dict = dict(zip(item_emb_df['article_id'], item_emb_np))
+    pickle.dump(item_emb_dict, open(save_path + 'item_content_emb.pkl', 'wb'))
+    
+    return item_emb_dict
+
+max_min_scaler = lambda x : (x-np.min(x))/(np.max(x)-np.min(x))
+# 全量训练集
+all_click_df = get_all_click_df(offline=False)
+# 对时间戳进行归一化,用于在关联规则的时候计算权重
+all_click_df['click_timestamp'] = all_click_df[['click_timestamp']].apply(max_min_scaler)
+item_info_df = get_item_info_df(data_path)
+item_emb_dict = get_item_emb_dict(data_path)
+
+print('load data success')
