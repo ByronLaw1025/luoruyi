47eab90c6af71db45fea59029f9089a757ee9d84
ByronLaw1025 <58861048+ByronLaw1025@users.noreply.github.com>
2022-05-14 19:52:54 +0800

    Add files via upload

---
+++ recall.py
@@ -0,0 +1,21 @@
+import pandas as pd  
+import numpy as np
+from tqdm import tqdm  
+from collections import defaultdict  
+import os, math, warnings, math, pickle
+from tqdm import tqdm
+import faiss
+import collections
+import random
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from datetime import datetime
+from deepctr.feature_column import SparseFeat, VarLenSparseFeat
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+
+from deepmatch.models import *
+from deepmatch.utils import sampledsoftmaxloss
+warnings.filterwarnings('ignore')
